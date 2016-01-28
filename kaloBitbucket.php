901b2757168c1f7d2f632a14662ce34c5b6270a6
Kaloyan <hlebarov@luckylabz.com>
2016-01-28 09:48:40 +0200

    Initial commit

---
+++ kaloBitbucket.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Kalo test plugin
+Plugin URI: http://kaloyanhlebarov.info/
+Description: A simple test plugin for bitbucket update
+Version: 1.0
+Author: Kaloyan Hlebarov
+Author URI: http://kaloyanhlebarov.info/
+*/
+
+add_action('init','helloKalo');
+
+function helloKalo() {
+    echo '<span style="color: darkred;">Hello master Kalo, may the force be with you, because you just updated the plugin!</span>';
+}
+
+require_once( 'BFIGitHubPluginUploader.php' );
+if ( is_admin() ) {
+    new BFIGitHubPluginUpdater( __FILE__, 'apxeonTepukc', "kalo-plugin" );
+}
