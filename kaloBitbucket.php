0386f60ca18a9f010c10440d88d66c04eca174d4
Kaloyan <hlebarov@luckylabz.com>
2016-01-28 11:56:29 +0200

    update

--- kaloBitbucket.php
+++ kaloBitbucket.php
@@ -8,13 +8,17 @@ Author: Harabejkov
 Author URI: http://harabejkov.info/
 */
 
-add_action('init','helloKalo');
-
-function helloKalo() {
-    echo '<span style="color: darkred;">Hello master Kalo, may the force be with you, because you just updated the plugin!</span>';
-}
-
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 require_once( 'BFIGitHubPluginUploader.php' );
 if ( is_admin() ) {
     new WPFDGitHubPluginUpdater( __FILE__, 'apxeonTepukc', "kalo-plugin-auto" );
+}
+
+/* The plugin itself */
+if (!is_admin()) {
+    add_action('init','helloKalo');
+}
+
+function helloKalo() {
+    echo '<span style="color: darkred;">Hello master Kalo, may the force be with you, because you just updated the plugin!</span>';
 }
